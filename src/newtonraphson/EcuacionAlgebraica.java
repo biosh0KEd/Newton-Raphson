eee7ed9bc1289360c9b583a8ebf1e09c9e28d695
Efrain Giron <efrainbond@hotmail.com>
2019-03-01 16:13:23 -0600

    Creado el metodo para derivar la ecuacion

--- src/newtonraphson/EcuacionAlgebraica.java
+++ src/newtonraphson/EcuacionAlgebraica.java
@@ -12,12 +12,20 @@ package newtonraphson;
 public class EcuacionAlgebraica {
     private final int numeroDeTerminos;
     private final double[][] termino;
+    private final double[][] terminoDerivado;
     public EcuacionAlgebraica(int term) {
         this.numeroDeTerminos = term;
         termino = new double[term][2];
+        terminoDerivado = new double[term][2];
     }
     public void ingresarDatos(int term, double coeficiente, double exponente) {
         this.termino[term][0] = coeficiente;
         this.termino[term][1] = exponente;
+    }
+    public void generarDerivada() {
+        for (int x = 0; x < this.numeroDeTerminos; x++){
+            terminoDerivado[x][0] = this.termino[x][0] * this.termino[x][1];
+            terminoDerivado[x][1] = this.termino[x][1] - 1;
+        }
     }
 }
